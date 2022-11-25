889df9ccc565e359459b0ed9a12be4c6df128bde
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-11-25 21:13:14 +0100

    fix reproducibility

--- src/bpe_models/random.py
+++ src/bpe_models/random.py
@@ -2,8 +2,10 @@ from .base import BaseBPE
 import random
 
 class RandomBPE(BaseBPE):
+    # 
     def __init__(self, seed=0, **kwargs):
         self.random = random.Random(seed)
+        print("Seeding", seed)
 
     def choose_pair_to_merge(self, pairs):
         return self.random.choice(list(pairs.keys()))
--- src/patches/02-submit_fit_bpe.sh
+++ src/patches/02-submit_fit_bpe.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/bash
 
 for VOCAB_SIZE in "4096" "8192" "16384"; do
-    for BPE_MODEL in "greedy" "random" "antigreedy"; do
-        echo "Submitting ${BPE_MODEL}_${VOCAB_SIZE}";
+    for BPE_MODEL in "greedy" "antigreedy"; do
+        SIGNATURE="${BPE_MODEL}_${VOCAB_SIZE}"
+        echo "Submitting ${SIGNATURE}";
         sbatch --time=1-0 --ntasks=20 --mem-per-cpu=2G \
-            --output="logs/fit_bpe_${BPE_MODEL}_${VOCAB_SIZE}.log" \
-            --job-name="fit_bpe_${BPE_MODEL}_${VOCAB_SIZE}" \
+            --output="logs/fit_bpe_${SIGNATURE}.log" \
+            --job-name="fit_bpe_${SIGNATURE}" \
             --wrap="python3 ./src/fit_bpe.py \
                 --model $BPE_MODEL \
+                --vocab-output computed/$BPE_MODEL.bpe_model \
+                --vocab-size $VOCAB_SIZE \
+                --number-of-lines 100000 \
+            ";
+    done;
+
+    BPE_MODEL="random"
+    for SEED in "0" "1" "2" "3" "4"; do
+        SIGNATURE="${BPE_MODEL}_${SEED}_${VOCAB_SIZE}"
+        echo "Submitting ${SIGNATURE}";
+        sbatch --time=1-0 --ntasks=20 --mem-per-cpu=2G \
+            --output="logs/fit_bpe_${SIGNATURE}.log" \
+            --job-name="fit_bpe_${SIGNATURE}" \
+            --wrap="python3 ./src/fit_bpe.py \
+                --model $BPE_MODEL \
+                --seed $SEED \
                 --vocab-output computed/$BPE_MODEL.bpe_model \
                 --vocab-size $VOCAB_SIZE \
                 --number-of-lines 100000 \
