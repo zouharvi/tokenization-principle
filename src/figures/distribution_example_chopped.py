d522be3ded5a1814d9fa87200aa12ea098812fb3
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-01-12 03:20:11 +0100

    nicer examples

---
+++ src/figures/distribution_example_chopped.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import fig_utils
+import matplotlib.pyplot as plt
+import argparse
+import numpy as np
+
+# for f in "0" "1" "2" "3"; do DISPLAY="" ./src/figures/distribution_example_chopped.py -f $f; done
+
+args = argparse.ArgumentParser()
+args.add_argument("-f", type=int, default=0)
+args = args.parse_args()
+
+plt.figure(figsize=(2, 1.2))
+
+if args.f == 0:
+    data_y = np.array([30, 15, 14, 14, 13, 12])
+elif args.f == 1:
+    data_y = np.array([30, 15, 14, 14, 13, 12, 4, 4, 2, 2, 2, 2])
+elif args.f == 2:
+    data_y = np.array([45, 40, 20, 20, 11, 10])
+elif args.f == 3:
+    data_y = np.array([45, 35, 14, 14, 13, 12, 11, 11, 9, 8, 5, 3])
+
+
+data_y = data_y / data_y.sum()
+data_x = np.array(range(len(data_y)))
+
+plt.bar(
+    data_x,
+    data_y,
+    width=1,
+    edgecolor="black",
+    color=fig_utils.COLORS[1],
+)
+
+plt.xlim(data_x.min()-0.5, data_x.max()+0.5)
+plt.ylim(0, 0.35)
+plt.xticks([])
+plt.yticks([])
+
+def renyi_log(data, power):
+    scale = 1/(1-power)
+    return scale*np.log2(np.sum(np.power(data, power)))
+
+def entropy(data):
+    data = np.array(data)
+    return -np.sum(data * np.log2(data))
+
+h1 = entropy(data_y)
+eff_h1 = entropy(data_y)/np.log2(len(data_y))
+
+plt.title(f"$V={len(data_y):.0f}$, H=${h1:.2f}$, Eff=${eff_h1:.0%}$%", fontsize=8)
+plt.ylabel("  Frequency" if args.f in {0, 2, 3} else r" ")
+
+plt.xlabel(
+    ["Efficient", "Inefficient", "Inefficient", "Mediocre"]
+    [args.f]
+)
+plt.tight_layout(pad=0.5)
+plt.savefig(f"computed/figures/distribution_example_chopped_f{args.f}.pdf")
+plt.show()
--- src/figures/distribution_example_spline.py
+++ src/figures/distribution_example_spline.py
@@ -27,17 +27,19 @@ elif args.f == 3:
 data_y = data_y / data_y.sum()
 data_x = np.array(range(len(data_y)))
 data_y_spline = make_interp_spline(data_x, data_y)
-vals_x = np.linspace(data_x.min(), data_x.max(), 500)
+vals_x = np.linspace(data_x.min(), data_x.max(), 10)
 vals_y = sorted(data_y_spline(vals_x), reverse=True)
 
-plt.fill_between(
+plt.bar(
     vals_x,
-    np.zeros_like(vals_x),
     vals_y,
+    edgecolor="black",
+    width=0.555,
+    color=fig_utils.COLORS[0]
 )
 
-plt.ylim(0, 0.5)
-plt.xlim(data_x.min(), data_x.max())
+plt.ylim(0, 0.6)
+plt.xlim(data_x.min()-0.22, data_x.max()+0.22)
 plt.xticks([])
 plt.yticks([])
 
@@ -56,7 +58,7 @@ h_0 = renyi_log(data_y, 0.0)
 
 print(h_i)
 
-plt.title(f"$H_3={h_3:.2f}, H_1={h_1:.2f}, H_0={h_0:.2f}$\n$H_\\infty={h_i:.2f},\,\,\, H_3/H_0={h_3/h_0:.0%}$%", fontsize=8)
+plt.title(f"$H_3={h_3:.2f}, H_1={h_1:.2f}, H_0={h_0:.2f}$\n$H_3/H_0={h_3/h_0:.0%}$%", fontsize=8)
 plt.ylabel("  Frequency" if args.f in {0, 2} else r" ")
 
 plt.xlabel(
