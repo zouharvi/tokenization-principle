fc6ffd792c42da8fd9fbdfa0f729b2cd12f73fcd
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-05-24 17:49:56 +0200

    .

--- src/patches/42-compute_test_scores.py
+++ src/patches/42-compute_test_scores.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 import glob
-import evalu
+import collections
 
 data = {
-    "cs-en": {},
-    "de-en": {},
+    "cs-en": collections.defaultdict([]),
+    "de-en": collections.defaultdict([]),
 }
 
+first = False
 for f in glob.glob("data_bin_ckpt_repl/*/model_bpe_random/*/dev_out.en"):
-    print(f)
+    pass
+
+for f in glob.glob("data_bin_ckpt_repl/*/model_bpe_random/*/dev_out.en"):
+    fs = f.split("/")
+    lang = fs[1]
+    signature = fs[-2]
+    print(lang, signature)
